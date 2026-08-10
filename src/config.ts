c7caf3a08bad904656ededae5d138a12dc2df6fa
christinalizx <christinalizx@gmail.com>
2026-08-10 16:55:13 -0400

    network traffic

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,6 @@
 
 export const SITE_TITLE = "Christina's Personal Website";
 export const SITE_DESCRIPTION = 'Welcome to my personal website, nice to meet you!';
+export const GOOGLE_ANALYTICS_ID = 'G-SNMSF2GEKF';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
